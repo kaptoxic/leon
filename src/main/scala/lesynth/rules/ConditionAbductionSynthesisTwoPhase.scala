37ae10a07cdf461978abc98328b1add128645683
Ivan Kuraj <ivan.kuraj@epfl.ch>
2013-04-11 01:02:46 +0200

    Updated rule to generate input examples + refactorings and fixes of code/testcases

--- src/main/scala/lesynth/rules/ConditionAbductionSynthesisTwoPhase.scala
+++ src/main/scala/lesynth/rules/ConditionAbductionSynthesisTwoPhase.scala
@@ -10,12 +10,9 @@ import leon.purescala.Definitions._
 import leon.purescala.DataGen.findModels
 import leon.synthesis._
 import leon.solvers.{ Solver, TimeoutSolver }
+import leon.evaluators.CodeGenEvaluator
 
 import InputExamples._
-import lesynth.SynthesizerForRuleExamples
-import leon.evaluators.DefaultEvaluator
-import insynth.leon.loader.LeonLoader
-import leon.evaluators.CodeGenEvaluator
 
 case object ConditionAbductionSynthesisTwoPhase extends Rule("Condition abduction synthesis (two phase).") {
   def instantiateOn(sctx: SynthesisContext, p: Problem): Traversable[RuleInstantiation] = {
@@ -40,13 +37,11 @@ case object ConditionAbductionSynthesisTwoPhase extends Rule("Condition abductio
                 val freshResID = FreshIdentifier("result").setType(holeFunDef.returnType)
                 val freshResVar = Variable(freshResID)
                 
-                val evaluator = new CodeGenEvaluator(sctx.context, program)
-                val models = findModels(p.pc, evaluator, 40, 2000, forcedFreeVars = Some(holeFunDef.args.map(_.id)))
-                
-							  def getInputExamples(argumentIds: Seq[Identifier], loader: LeonLoader) = {
-                  //println(models.toList)
-							    models.toList
-							  }   
+                val codeGenEval = new CodeGenEvaluator(sctx.context, sctx.program)
+                def getInputExamples = 
+                  getDataGenInputExamples(codeGenEval, p, 
+                		40, 2000, Some(holeFunDef.args.map(_.id))
+                	) _
                 
                 holeFunDef.postcondition = Some(replace(
                   Map(givenVariable.toVariable -> ResultVariable().setType(holeFunDef.returnType)), p.phi))
