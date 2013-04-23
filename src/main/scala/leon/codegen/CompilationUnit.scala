2b1e9c5e4c0677b87133b506e6f32b9841e4f3de
Philippe Suter <philippe.suter@gmail.com>
2013-04-23 14:28:04 +0200

    Contract checking now optional in CodeGenEvaluator.

--- src/main/scala/leon/codegen/CompilationUnit.scala
+++ src/main/scala/leon/codegen/CompilationUnit.scala
@@ -140,7 +140,7 @@ class CompilationUnit(val program: Program, val classes: Map[Definition, ClassFi
     val m = cf.addMethod(
       typeToJVM(e.getType),
       "eval",
-      (("L" + CodeGeneration.TickerClass + ";") +:
+      (("L" + CodeGeneration.MonitorClass + ";") +:
        args.map(a => typeToJVM(a.getType))) : _*
     )
 
@@ -218,7 +218,7 @@ object CompilationUnit {
       val m = cf.addMethod(
         typeToJVM(funDef.returnType),
         mn,
-        (("L" + CodeGeneration.TickerClass + ";") +:
+        (("L" + CodeGeneration.MonitorClass + ";") +:
          funDef.args.map(a => typeToJVM(a.tpe))) : _*
       )
       m.setFlags((
