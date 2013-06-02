ac37cae6ff82c18e2d6ab455873b8e539254ade2
Etienne Kneuss <colder@php.net>
2013-06-02 03:22:52 +0200

    Bleh

--- allExamples.sh
+++ allExamples.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 # Standard examples:
+echo "====================================================================================="
+echo "/!\\/!\\/!\\ Make sure the web-server is not up when running leon from the CLI /!\\/!\\/!\\"
+echo "====================================================================================="
+echo "Press enter to run examples..."
+read a
 
 FILES=`find oopsla-examples/{Church,List,SortedList,StrictSortedList} -name "*.scala" | sort`
 
-#for f in $FILES; do
-#    echo "===================================================================================="
-#    echo "Running on $f..."
-#    echo "===================================================================================="
-#    cat $f
-#    echo "Press enter to start synthesis..."
-#    read a
-#    ./leon --synthesis --cegis:gencalls $f
-#    echo "Press enter to continue to the next problem..."
-#    read a
-#done
+for f in $FILES; do
+    echo "===================================================================================="
+    echo "Running on $f..."
+    echo "===================================================================================="
+    cat $f
+    echo "Press enter to start synthesis..."
+    read a
+    ./leon --synthesis --cegis:gencalls $f
+    echo "Press enter to continue to the next problem..."
+    read a
+done
 
 FILES2=`find oopsla-examples/{InsertionSort,MergeSort} -name "*.scala" | sort`
 
