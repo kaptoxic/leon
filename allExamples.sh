9de6491d1f760e030a96f747388b51e06b74f777
Etienne Kneuss <colder@php.net>
2013-06-02 02:07:17 +0200

    Add script and examples foor oopsla artifact

---
+++ allExamples.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Standard examples:
+
+FILES=`find oopsla-examples/{Church,List,SortedList,StrictSortedList} -name "*.scala" | sort`
+
+for f in $FILES; do
+    echo "===================================================================================="
+    echo "Running on $f..."
+    echo "===================================================================================="
+    cat $f
+    echo "Press enter to start synthesis..."
+    read a
+    ./leon --synthesis --cegis:gencalls $f
+    echo "Press enter to continue to the next problem..."
+    read a
+done
+
+FILES2=`find oopsla-examples/{InsertionSort,MergeSort} -name "*.scala" | sort`
+
+
+for f in $FILES; do
+    echo "===================================================================================="
+    echo "Running on $f..."
+    echo "===================================================================================="
+    cat $f
+    echo "Press enter to start synthesis..."
+    read a
+    ./leon --costmodel=naive --synthesis --cegis:gencalls $f
+    echo "Press enter to continue to the next problem..."
+    read a
+done
