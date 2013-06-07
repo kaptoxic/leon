02b52cda44ddf3799eecb4433adae54876b9ccf2
kuraj <ivan.kuraj@epfl.ch>
2013-06-07 18:04:28 +0200

    fixes for the strategy include

--- src/main/scala/lesynth/examples/Example.scala
+++ src/main/scala/lesynth/examples/Example.scala
@@ -5,4 +5,9 @@ import leon.purescala.Trees._
 import leon.purescala.Common.{ Identifier, FreshIdentifier }
 
 case class Example(map: Map[Identifier, Expr]) {
+  
+  override def toString = {
+    map.toString
+  }
+  
 }
