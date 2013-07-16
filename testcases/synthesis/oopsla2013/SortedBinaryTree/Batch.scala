cb75334fd8e0abed20355f860d550791e0442ffd
Philippe Suter <philippe.suter@gmail.com>
2013-07-16 21:29:04 +0200

    fixed isSorted

--- testcases/synthesis/oopsla2013/SortedBinaryTree/Batch.scala
+++ testcases/synthesis/oopsla2013/SortedBinaryTree/Batch.scala
@@ -5,35 +5,42 @@ import leon.Utils._
 object BinaryTree {
   sealed abstract class Tree
   case class Node(left : Tree, value : Int, right : Tree) extends Tree
-  case class Leaf() extends Tree
+  case object Leaf extends Tree
 
   def content(t : Tree): Set[Int] = t match {
-    case Leaf() => Set.empty[Int]
+    case Leaf => Set.empty[Int]
     case Node(l, v, r) => content(l) ++ Set(v) ++ content(r)
   }
   
-  sealed abstract class OptInt
-  case class Some(value : Int) extends OptInt
-  case object None extends OptInt
+  sealed abstract class OptPair
+  case class Pair(v1 : Int, v2 : Int) extends OptPair
+  case object NoPair extends OptPair
   
-  def isSortedX(t : Tree) : (Boolean, OptInt, OptInt) = (t match {
-    case Leaf() => (true, None, None)
-    
+  def isSortedX(t : Tree) : (Boolean, OptPair) = (t match {
+    case Leaf => (true, NoPair)
+       
     case Node(l, v, r) =>
-      val (ls,ll,lu) = isSortedX(l)
-      val (rs,rl,ru) = isSortedX(r)
-            
-      val (lOK,newMin) = lu match {
-        case None => (ls, v)
-        case Some(v2) => (ls && v2 < v, v2)
+      val (ls,lb) = isSortedX(l)
+      val (rs,rb) = isSortedX(r)
+               
+      val (lOK,newMin) = lb match {
+        case NoPair => (ls, v)
+        case Pair(ll, lh) => (ls && lh < v, ll)
       }
-      
-      val (rOK,newMax) = rl match {
-        case None => (rs,v)
-        case Some(v2) => (rs && v < v2, v2)
+         
+      val (rOK,newMax) = rb match {
+        case NoPair => (rs, v)
+        case Pair(rl, rh) => (rs && v < rl, rh)
       }
-      
-      (lOK && rOK, Some(newMin), Some(newMax))
+
+    if(lOK && rOK) {
+      (true, Pair(newMin, newMax))
+    } else {
+      (false, NoPair)
+    }
+  }) ensuring((res : (Boolean,OptPair)) => res match {
+    case (s, Pair(l,u)) => s && (l <= u)
+    case _ => true  
   })
 
   def isSorted(t: Tree): Boolean = isSortedX(t)._1
@@ -42,6 +49,16 @@ object BinaryTree {
     (out : Tree) => content(out) == (content(in) -- Set(v))
   }
 
+  // def insertImpl(t : Tree, x : Int) : Tree = {
+  //   require(isSorted(t))
+  //   t match {
+  //    case Leaf => Node(Leaf, x, Leaf)
+  //    case Node(l, v, r) if v == x => Node(l, v, r)
+  //    case Node(l, v, r) if x < v  => Node(insertImpl(l, x), v, r)
+  //    case Node(l, v, r) if v < x  => Node(l, v, insertImpl(r, x))
+  //   }
+  // } ensuring(isSorted(_))
+ 
   def insertSynth(in : Tree, v : Int) = choose {
     (out : Tree) => content(out) == (content(in) ++ Set(v))
   }
