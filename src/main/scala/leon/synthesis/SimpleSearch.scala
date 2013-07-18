477f628338eedccf137d83f2c3d71a63fe2d3575
Etienne Kneuss <colder@php.net>
2013-07-18 20:41:42 +0200

    Let's just say this is a hack

--- src/main/scala/leon/synthesis/SimpleSearch.scala
+++ src/main/scala/leon/synthesis/SimpleSearch.scala
@@ -6,6 +6,7 @@ package synthesis
 import purescala.Definitions.FunDef
 
 import synthesis.search._
+import lesynth.rules._
 
 case class TaskRunRule(app: RuleInstantiation) extends AOAndTask[Solution] {
 
@@ -47,7 +48,7 @@ class SimpleSearch(synth: Synthesizer,
 
   val sctx = SynthesisContext.fromSynthesizer(synth)
 
-  def expandAndTask(t: TaskRunRule): ExpandResult[TaskTryRules] = {
+  def expandAndTask(t: TaskRunRule, pathToRoot: List[RuleInstantiation]): ExpandResult[TaskTryRules] = {
     val prefix = "[%-20s] ".format(Option(t.rule).getOrElse("?"))
 
     info(prefix+"Got: "+t.problem)
@@ -71,7 +72,7 @@ class SimpleSearch(synth: Synthesizer,
     }
   }
 
-  def expandOrTask(t: TaskTryRules): ExpandResult[TaskRunRule] = {
+  def expandOrTask(t: TaskTryRules, pathToRoot: List[RuleInstantiation]): ExpandResult[TaskRunRule] = {
     val (normRules, otherRules) = (Seq[Rule](), rules)//.partition(r => r.isInstanceOf[NormalizingRule] || r.name.contains("abduction"))
 
     val normApplications = normRules.flatMap(_.instantiateOn(sctx, t.p))
@@ -80,7 +81,11 @@ class SimpleSearch(synth: Synthesizer,
       Expanded(List(TaskRunRule(normApplications.head)))
     } else {
       val sub = otherRules.flatMap { r =>
-        r.instantiateOn(sctx, t.p).map(TaskRunRule(_))
+        if (r == ConditionAbductionSynthesisTwoPhase && !pathToRoot.forall(_.rule.isInstanceOf[NormalizingRule])) {
+          None
+        } else {
+          r.instantiateOn(sctx, t.p).map(TaskRunRule(_))
+        }
       }
 
       if (!sub.isEmpty) {
@@ -186,15 +191,28 @@ class SimpleSearch(synth: Synthesizer,
   }
 
 
+  def pathToRoot(l: g.Tree): List[RuleInstantiation] = {
+    if (l.parent eq null) {
+      Nil
+    } else {
+      l.parent.task match {
+        case t: TaskRunRule =>
+          t.app :: pathToRoot(l.parent)    
+        case _ =>
+          pathToRoot(l.parent)
+      }
+    }
+  }
+
   def searchStep() {
     nextLeaf() match {
       case Some(l)  =>
         l match {
           case al: g.AndLeaf =>
-            val sub = expandAndTask(al.task)
+            val sub = expandAndTask(al.task, pathToRoot(al))
             onExpansion(al, sub)
           case ol: g.OrLeaf =>
-            val sub = expandOrTask(ol.task)
+            val sub = expandOrTask(ol.task, pathToRoot(ol))
             onExpansion(ol, sub)
         }
       case None =>
