373f2ad46e2e049a9a8dbe2bd961836c498a0df5
Ivan Kuraj <ivan.kuraj@epfl.ch>
2013-11-19 16:10:41 +0100

    Added sorting of examples

--- src/test/scala/leon/test/ioexamples/FragmenterTest.scala
+++ src/test/scala/leon/test/ioexamples/FragmenterTest.scala
@@ -23,33 +23,33 @@ class EvaluationTest extends FunSuite {
   
   test("(A B) subexpressions") {
     
-    val map = allSubexpressions(ieLP_AB_RP)
+    val map = allSubexpressions(elABr)
         
     map.size should be (5)
     
-    for(subexp <- List(varA, varB, Cons(varB, nil), nil, ieLP_AB_RP))
+    for(subexp <- List(varA, varB, Cons(varB, nil), nil, elABr))
       map contains subexp
   }
   
   test("(A B) map to subexpressions") {
     
-    val map = mapOfSubexpressions(ieLP_AB_RP)
+    val map = mapOfSubexpressions(elABr)
         
     map.size should be (5)
     
-    val x = ieLP_AB_RP
+    val x = elABr
     for((subtree, ops) <- List(
       (varA, Car(x)), (varB, Car(Cdr(x))), (Cons(varB, nil), Cdr(x)),
-      (nil, Cdr(Cdr(x))), (ieLP_AB_RP, x)
+      (nil, Cdr(Cdr(x))), (elABr, x)
     ))
       map(subtree) should be (ops)
   }
   
   test("(A B) fragment") {
-    val x = ieLP_AB_RP
+    val x = elABr
     
     expectResult( Cons(Cons(Car(x), nil), Cons(Cdr(x), nil)) ) {
-      constructFragment(oeLP_AB_RP, mapOfSubexpressions(ieLP_AB_RP))
+      constructFragment(ellArlBrr, mapOfSubexpressions(elABr))
     }
   }
   
@@ -62,6 +62,54 @@ class EvaluationTest extends FunSuite {
     res(0)(nil) should be (Cdr(Car(nil)))
 //    res(0)((ellABrlBlABrr)) should be (List(varB))
 //    res(0)((ellABrllABrlABrr)) should be (List(ieLP_AB_RP))
+  }
+
+  test("compare trees") {
+    implicit val map = Map[(Expr, Expr), Int]()
+
+    def compRes(e1: Expr, e2: Expr) = compare(e1, e2)._1
+    
+    compRes(varA, varB) should be (0)
+    compRes(varA, varA) should be (0)
+    compRes(elABr, varA) should be (1)
+    compRes(varA, elABr) should be (-1)
+    compRes(varA, ellArlBrr) should be (-1)
+    compRes(ellArlBrr, elABr) should be (1)
+    compRes(ellABrllABrlABrr, ellABrlBlABrr) should be (1)
+    compRes(ellABrlBlABrr, elBlABrlABrr) should be (0)
+    compRes(elBlABrlABrr, ellABrlBlABrr) should be (0)
+  }
+
+  test("compare trees maps") {
+    implicit var map = Map[(Expr, Expr), Int]()
+
+    def compRes(e1: Expr, e2: Expr) = {
+      assert ( compare(e1, e2)._2.size >= map.size)
+
+      map = compare(e1, e2)._2
+      compare(e1, e2)._1
+    }
+    
+    compRes(varA, varB) should be (0)
+    compRes(varA, varA) should be (0)
+    compRes(elABr, varA) should be (1)
+    compRes(varA, elABr) should be (-1)
+    compRes(varA, ellArlBrr) should be (-1)
+    compRes(ellArlBrr, elABr) should be (1)
+    compRes(ellABrllABrlABrr, ellABrlBlABrr) should be (1)
+    compRes(ellABrlBlABrr, elBlABrlABrr) should be (0)
+    compRes(elBlABrlABrr, ellABrlBlABrr) should be (0)
+  }
+
+  test("sorting") {
+    val inputExamples = List(nil, elAr, elABr, elABCr)
+    
+    
+    for (permutation <- inputExamples.permutations) {
+      assert(permutation.size == inputExamples.size)
+      
+      sort(permutation) should be (inputExamples)
+    }
   }
 
 }
