bb08fd8c12130c7636fbe6fe21ce90b9ebbbbe6c
Ivan Kuraj <ivan.kuraj@epfl.ch>
2013-11-20 01:19:01 +0100

    Added finding of chains

--- src/main/scala/leon/synthesis/ioexamples/Util.scala
+++ src/main/scala/leon/synthesis/ioexamples/Util.scala
@@ -2,18 +2,42 @@ package leon
 package synthesis.ioexamples
 
 import purescala.{ Trees => ot }
-import purescala.Trees.Expr
+import purescala.Trees.{ Cons => _, Car => _, Cdr => _, _ }
 import purescala.TypeTrees._
 import purescala.TreeOps
+import purescala.Common.FreshIdentifier
+import purescala.Extractors
 
 object Util {
+  
+  import Extractors._
+  import TreeOps._
   
   // avoid typing problems
   val listType = ListType(BooleanType)
   
+  val w = Variable(FreshIdentifier("w")).setType(listType)
+  val nil = ot.NilList(listType)
+  
   def Cons(h: Expr, t: Expr) = ot.Cons(h, t).setType(listType)
-  def Nil = ot.NilList(listType)
   def Car(l: Expr) = ot.Car(l).setType(listType)
   def Cdr(l: Expr) = ot.Cdr(l).setType(listType)
+  
+  def isAtom(e: Expr) = e match {
+	  case Atom(_) => true
+	  case _ => false
+	}
+
+  def substituteAtom(e: Expr) = e match {
+	  case Atom(_) => Some(w)
+	  case _ => None
+	}
+  
+  def substituteAllAtom(e: Expr) = searchAndReplace( substituteAtom )(e)
+
+//  def substituteAtom(e: Expr) = e match {
+//	  case Atom(_) => w
+//	  case _ => e
+//	}
 
 }
