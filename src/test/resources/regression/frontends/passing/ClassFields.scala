fcaacaae6496985280517201ce6ad2b96d3bbd61
Ivan Kuraj <ivan.kuraj@gmail.com>
2016-04-04 16:55:02 -0400

    Make Eclipse not complain about incomplete tests

--- src/test/resources/regression/frontends/passing/ClassFields.scala
+++
@@ -1,6 +0,0 @@
-import leon.lang._
-
-object ClassFields {
-  case class Foo(f : Option[Foo])
-}
-
--- src/test/resources/regression/verification/purescala/valid/CallByName1.scala
+++
@@ -1,9 +0,0 @@
-import leon.lang._
-
-object CallByName1 {
-  def add(a: => Int, b: => Int): Int = a + b
-
-  def test(): Int = {
-    add(1,2)
-  } ensuring (_ == 3)
-}
