108b9b5f61e2fbb688edef6a98fcc42d8616f77a
Ivan Kuraj <ivan.kuraj@gmail.com>
2017-03-19 00:44:37 -0400

    Change logging to be lazy

--- src/main/scala/leon/utils/logging/ScalaLog4jLogger.scala
+++ src/main/scala/leon/utils/logging/ScalaLog4jLogger.scala
@@ -1,29 +1,36 @@
 package leon.utils.logging
 
-import org.apache.logging.log4j.{ Logger => Logger4J }
+import org.apache.logging.log4j.{ Logger => Logger4J, Level }
 
 class ScalaLog4jLogger(logger: Logger4J) extends Logger {
 
   protected[this] def getMyClass = this.getClass
-    
-  def severe(msg: => String)(implicit hasLogger: HasLogger) = logger.error("FATAL:\n" + msg)
-
-  def error(msg: => String)(implicit hasLogger: HasLogger) = logger.error(msg)
   
-  def warning(msg: => String)(implicit hasLogger: HasLogger) = logger.warn(msg)  
+  def log(msg: => String, lvl: Level) =
+    if (logger.isEnabled(lvl)) logger.log(lvl, msg)
+    
+  import Level._
+    
+  def severe(msg: => String)(implicit hasLogger: HasLogger) =
+    log(msg, FATAL)
+//    logger.error("FATAL:\n" + msg)
+
+  def error(msg: => String)(implicit hasLogger: HasLogger) = log(msg, ERROR)
+  
+  def warning(msg: => String)(implicit hasLogger: HasLogger) = log(msg, WARN)  
      
-  def info(msg: => String)(implicit hasLogger: HasLogger) = logger.info(msg)
+  def info(msg: => String)(implicit hasLogger: HasLogger) = log(msg, INFO)
    
-  def fine(msg: => String)(implicit hasLogger: HasLogger) = logger.debug(msg)
+  def fine(msg: => String)(implicit hasLogger: HasLogger) = log(msg, DEBUG)
    
-  def finer(msg: => String)(implicit hasLogger: HasLogger)  = logger.debug(msg)
+  def finer(msg: => String)(implicit hasLogger: HasLogger)  = log(msg, DEBUG)
    
-  def finest(msg: => String)(implicit hasLogger: HasLogger) = logger.trace(msg)
+  def finest(msg: => String)(implicit hasLogger: HasLogger) = log(msg, TRACE)
      
   def entering(method: => String, arguments: Any*)(implicit hasLogger: HasLogger) =
-	  logger.trace("Entering " + method + " with " + arguments.mkString(", "))
+	  log("Entering " + method + " with " + arguments.mkString(", "), TRACE)
      
   def exiting(method: => String, result: => String)(implicit hasLogger: HasLogger) =
-	  logger.trace("Exiting " + method + " with " + result)
+	  log("Exiting " + method + " with " + result, TRACE)
 
 }
